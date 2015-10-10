6e6db8c6433a3e79c9f0bacaa94bbd0550445aa5
ipc103 <iancandysemail@gmail.com>
2015-10-10 13:09:06 -0400

    modify canvas size and slider position

--- app/sketch.js
+++ app/sketch.js
@@ -1,16 +1,21 @@
-var slider;
+var rslider, bslider, gslider;
 
 function setup() {
   // uncomment this line to make the canvas the full size of the window
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, 500);
   colorMode(HSB, 255);
-  slider = createSlider(0, 255, 255);
+  rslider = createSlider(0, 255, 100);
+  gslider = createSlider(0, 255, 55);
+  bslider = createSlider(0, 255, 255);
+  rslider.position(0, 550);
+  gslider.position(200, 550);
+  bslider.position(400, 550);
 }
 
 function draw() {
   // draw stuff here
   // stroke(slider.value(), 255, 255);
-  fill(slider.value(), 255, 255, 0);
+  fill(rslider.value(), gslider.value(), bslider.value(), 0);
   if ( mouseIsPressed ) {
     ellipse(mouseX, mouseY, 80, 80);
   } 
