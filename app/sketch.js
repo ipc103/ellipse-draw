5a8e5dbc48b8d36d8200367d7b72914349cd293c
ipc103 <iancandysemail@gmail.com>
2015-10-10 14:07:01 -0400

    add save button

--- app/sketch.js
+++ app/sketch.js
@@ -3,6 +3,7 @@ var rslider, bslider, gslider;
 function setup() {
   // uncomment this line to make the canvas the full size of the window
   createCanvas(windowWidth, 500);
+  background(255);
   colorMode(HSB, 255);
   rslider = createSlider(0, 255, 100);
   gslider = createSlider(0, 255, 55);
@@ -10,6 +11,9 @@ function setup() {
   rslider.position(0, 550);
   gslider.position(200, 550);
   bslider.position(400, 550);
+  var saveButton = createButton("Save");
+  saveButton.position(600, 550);
+  saveButton.mousePressed(saveImage);
 }
 
 function draw() {
@@ -18,6 +22,14 @@ function draw() {
   fill(rslider.value(), gslider.value(), bslider.value(), 0);
   if ( mouseIsPressed ) {
     ellipse(mouseX, mouseY, 80, 80);
+  } 
+  
+}
+
+function saveImage() {
+  if ( confirm("Download a copy?") === true ) {
+    var name = prompt("Enter a filename:", "ellipses");
+    save(name);
   } 
   
 }
