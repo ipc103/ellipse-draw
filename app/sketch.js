cae3c45ecd5792bd5e304efaca13657e6be0e807
ipc103 <iancandysemail@gmail.com>
2015-10-11 20:38:23 -0400

    add size slider

--- app/sketch.js
+++ app/sketch.js
@@ -1,4 +1,4 @@
-var rslider, bslider, gslider;
+var rslider, bslider, gslider, sizeSlider;
 
 function setup() {
   // uncomment this line to make the canvas the full size of the window
@@ -8,20 +8,23 @@ function setup() {
   rslider = createSlider(0, 255, 0);
   gslider = createSlider(0, 255, 200);
   bslider = createSlider(0, 255, 200);
+  sizeSlider = createSlider(0, 100, 80);
   rslider.position(0, 550);
   gslider.position(200, 550);
   bslider.position(400, 550);
+  sizeSlider.position(600, 550);
   var saveButton = createButton("Save");
-  saveButton.position(600, 550);
+  saveButton.position(800, 550);
   saveButton.mousePressed(saveImage);
 }
 
 function draw() {
   // draw stuff here
   // stroke(slider.value(), 255, 255);
+  var size = sizeSlider.value();
   fill(rslider.value(), gslider.value(), bslider.value(), 0);
   if ( mouseIsPressed ) {
-    ellipse(mouseX, mouseY, 80, 80);
+    ellipse(mouseX, mouseY, size, size);
   } 
   
 }
